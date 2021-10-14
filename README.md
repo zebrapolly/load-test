9556ab1ff4fc009d9d706854326c7d3ef872b58a
Mikhail Smirnov <zebrapolly@gmail.com>
2021-10-14 11:46:47 +0300

    add load testing

--- README.md
+++ README.md
@@ -9,14 +9,32 @@ and check efficiency on simple ecommerce service.
 - searching products
 - putting product to card
 
+### Pitfalls
+I store same data in different places(products and users logins in
+root folder and in api folder. I need these datasets as a storaged 
+information and sets for the testing), because of conternization 
+and docker-compose context. I'll fix it when I start to use 
+databases with migrations and create more advanced infrastructure 
+approaches.
+
 ## How I test
 
-I use artillery.io for creating and evaluating scenarios
+I use [k6.io](https://k6.io/) for creating and evaluating test scenarios.
+All results are storaged in influxds with grafana ui.
+In this step testing starts with next steps:
+ 1. Start service `cd api && nm run start:dev`
+ 2. Start db and ui fot test results `docker-compose up -d influxdb grafana `
+ 3. Start test `k6 run --out influxdb=http://localhost:8086/k6 scripts/search-and-add-to-cart.js` (you should install k6)
+
+### Grafana
+http://localhost:3001/
+user root
+pass root
 
 ## What technics I want to learn
-- http/2 - http/1
-- caching
 - database performance improvements
+- caching
 - scaling services
 - cqrs
+- http/2 - http/1
 
---
+++ load-test/scripts/search-and-add-to-cart.js
@@ -0,0 +1,110 @@
+import http from 'k6/http';
+import {  check, group, sleep } from 'k6';
+import { SharedArray } from "k6/data";
+
+export let options = {
+    stages: [
+        // { duration: '1m', target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+        // { duration: '10m', target: 60 }, // stay at 60 users for 10 minutes
+        { duration: '3m', target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+        { duration: '2m', target: 100 }, // stay at 100 users for short amount of time (peak hour)
+        { duration: '3m', target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+        { duration: '10m', target: 60 }, // continue at 60 for additional 10 minutes
+        { duration: '5m', target: 0 },
+    ],
+}
+const HOST = 'http://localhost:3000';
+const logins = new SharedArray("logins", function() { return JSON.parse(open('../../users.json')); });
+const searchVars = new SharedArray("keywords", function() { return JSON.parse(open('../datasets/keywords.json')); });
+
+export default function () {
+    group('search and pu to a card',(_) => {
+        let params = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            tags: {
+                name: 'login',
+            },
+        };
+
+        const body = logins[__VU];
+        const loginResponse = http.post(`${HOST}/auth/login`, JSON.stringify(body), params);
+        check(loginResponse, {
+            'login is status 200': (r) => r.status === 200,
+            'login is access token present': (r) => r.json().hasOwnProperty('access_token'),
+        });
+        const accessToken = loginResponse.json()['access_token'];
+
+        const randomQueryIndex = randomIntFromInterval(0, searchVars.length-1)
+        const searchQuery = `title=${searchVars[randomQueryIndex]}`;
+        const searchParams = {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            },
+            tags: {
+                name: 'search'
+            }
+        }
+        const searchResponse = http.get(`${HOST}/products/search?${searchQuery}`, searchParams);
+        check(searchResponse, {
+            'search is status 200': (r) => r.status === 200
+        });
+
+        const searchResponseVars = searchResponse.json();
+        const randomIndex = randomIntFromInterval(0, searchResponseVars.length-1);
+        const chosenVarId = searchResponseVars[randomIndex].id;
+        const getProductParams = {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            },
+            tags: {
+                name: 'getProduct'
+            }
+        }
+        const getProductResponse = http.get(`${HOST}/products/${chosenVarId}`, getProductParams);
+        check(getProductResponse, {
+            'getProduct is status 200': (r) => r.status === 200
+        });
+
+        const putToCardParams = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + accessToken
+            },
+            tags: {
+                name: 'putToCard'
+            }
+        }
+        const putToCardBody = {
+            productId: chosenVarId
+        }
+        const putToCardResponse = http.post(`${HOST}/cards`, JSON.stringify(putToCardBody), putToCardParams);
+        check(putToCardResponse, {
+            'putToCard is status 200': (r) => r.status === 200,
+            'putToCard is product in card': (r) => r.json().hasOwnProperty(chosenVarId)
+        });
+
+        const getCardParams = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + accessToken
+            },
+            tags: {
+                name: 'getCard'
+            }
+        }
+
+        const getCardResponse = http.get(`${HOST}/cards`, getCardParams);
+        check(getCardResponse, {
+            'getCard is status 200': (r) => r.status === 200,
+            'getCard is product in card': (r) => r.json().hasOwnProperty(chosenVarId)
+        });
+
+        sleep(1);
+    });
+}
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
